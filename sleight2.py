cf7de9fbbd53d3e62f95edc9fff364bd77ee78c4
cptsticky <81385287+cptsticky@users.noreply.github.com>
2021-08-12 10:02:51 -0500

    Update sleight2.py

--- sleight2.py
+++ sleight2.py
@@ -3,7 +3,7 @@
 
 ############################################################################################
 # sleight2.py:   Empire HTTP(S) C2 redirector setup script
-# Author:   cptsticky 
+# Author:   cptsticky
 ############################################################################################
 
 import subprocess
@@ -18,6 +18,9 @@ R = '\033[1;31m'  #red
 T = '\033[1;93m'  #tan
 G = '\033[32m'    #green
 LG = '\033[1;32m' #light green
+domain = ''
+redirect = ''
+redirector = ''
 
 htaccess_template = '''
 RewriteEngine On
@@ -117,13 +120,14 @@ def convert_profile():
             HTTPS = raw_input("[-] HTTPS listener? [y/N]: ")
 
     if args.redirectDomain:
+        global redirect
         redirect = args.redirectDomain
     else:
         redirect = raw_input(
         G + '[+]' + W + ' Redirect Site URL: ')
         while redirect == '':
             redirect = raw_input("[-] Redirect Site URL: ")
-
+      
     commProfile = open(args.commProfile, 'r')
     cp_file = commProfile.read()
     commProfile.close()
@@ -142,9 +146,9 @@ def convert_profile():
     # HTTPS rules
     if HTTPS == 'y':
         htaccess_template_https = htaccess_template.replace('http', 'https', 1)
-        rules = (htaccess_template_https.format(uri,user_agent,LHOST,LPORT,redirect))
+        rules = (htaccess_template_https.format(uri,user_agent,LHOST,LPORT,domain))
     else:
-        rules = (htaccess_template.format(uri,user_agent,LHOST,LPORT,redirect))
+        rules = (htaccess_template.format(uri,user_agent,LHOST,LPORT,domain))
 
     print LG + '\n[!]' + W + ' mod_rewrite rules generated.'
     print rules
@@ -223,7 +227,8 @@ def mod_rewrite_config(rules):
         ac2.close()
 
         # Enable mod_rewrite modules
-        subprocess.call(['a2enmod', 'rewrite', 'proxy', 'proxy_http'])
+        subprocess.call(['a2enmod', 'rewrite', 'proxy', 'proxy_http', 'proxy_connect'])
+        
 
         # HTTPS configuration
         f = re.split("\n", rules)
@@ -234,11 +239,69 @@ def mod_rewrite_config(rules):
                 print '\n' + T + '[*]' + W + ' Enabling HTTPS...\n'
                 subprocess.call(['a2enmod', 'ssl'])
                 subprocess.call(['a2ensite', 'default-ssl.conf'])
+                subprocess.call(['a2enmod', 'proxy_connect'])
                 # Backup SSL config file
                 if not os.path.isfile("/etc/apache2/sites-enabled/default-ssl.conf.bak"):
                         print '\n' + T + '[*]' + W + ' Backing up SSL configuration file...'
                         subprocess.call(['cp', '/etc/apache2/sites-enabled/default-ssl.conf', \
                                 '/etc/apache2/sites-enabled/default-ssl.conf.bak'])
+
+                # Edit LE SSL config file
+                le_config = '''
+                <IfModule mod_ssl.c>
+                <VirtualHost *:443>
+                        # The ServerName directive sets the request scheme, hostname and port that
+                        # the server uses to identify itself. This is used when creating
+                        # redirection URLs. In the context of virtual hosts, the ServerName
+                        # specifies what hostname must appear in the request's Host: header to
+                        # match this virtual host. For the default virtual host (this file) this
+                        # value is not decisive as it is used as a last resort host regardless.
+                        # However, you must set it for any further virtual host explicitly.
+                        #ServerName www.example.com
+
+                        ServerAdmin webmaster@localhost
+                        DocumentRoot /var/www/html
+
+                        # Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
+                        # error, crit, alert, emerg.
+                        # It is also possible to configure the loglevel for particular
+                        # modules, e.g.
+                        #LogLevel info ssl:warn
+
+                        ErrorLog /var/log/apache2/error.log
+                        CustomLog /var/log/apache2/access.log combined
+
+                        # For most configuration files from conf-available/, which are
+                        # enabled or disabled at a global level, it is possible to
+                        # include a line for only one particular virtual host. For example the
+                        # following line enables the CGI configuration for this host only
+                        # after it has been globally disabled with "a2disconf".
+                        #Include conf-available/serve-cgi-bin.conf
+                
+                
+                ServerName www.{}
+                Include /etc/letsencrypt/options-ssl-apache.conf
+                ServerAlias {}
+                SSLCertificateFile /etc/letsencrypt/live/{}/fullchain.pem
+                SSLCertificateKeyFile /etc/letsencrypt/live/{}/privkey.pem
+
+                SSLEngine On
+                # Enable Proxy
+                SSLProxyEngine On
+                # Trust Self-Signed Certificates
+                SSLProxyVerify none
+                SSLProxyCheckPeerCN off
+                SSLProxyCheckPeerName off
+
+                ProxyPass / https://{}/
+                ProxyPassReverse / https://{}/
+                </VirtualHost>
+                </IfModule>'''.format(domain, domain, domain, domain, redirect, redirect)
+
+
+                lessl = open('/etc/apache2/sites-enabled/000-default-le-ssl.conf', 'w')
+                lessl.write(le_config)
+                lessl.close()
 
                 # Edit SSL config file
                 ssl1 = open('/etc/apache2/sites-enabled/default-ssl.conf', 'r')
@@ -317,7 +380,7 @@ if __name__ == "__main__":
             `-----+'\  / | Y  J|
                   |  \/ J|-----'
                   `------'
-            
+
 ████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
 █░░░░░░░░░░░░░░█░░░░░░█████████░░░░░░░░░░░░░░█░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░██░░░░░░█░░░░░░░░░░░░░░████░░░░░░░░░░░░░░█
 █░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░█████████░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░████░░▄▀▄▀▄▀▄▀▄▀░░█
